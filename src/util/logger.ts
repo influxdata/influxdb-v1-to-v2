a3736be3781d71fa07b157fab69302425cd6ca15
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-02-05 07:11:18 +0100

    feat: use chalk to colorize output

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,33 +1,33 @@
 /* eslint-disable no-console */
-import colors from 'colors'
+import chalk from 'chalk'
 
 const TRACE_ENABLED = !!(process.env['TRACE'] && process.env['TRACE'] !== '0')
 
 const logger = {
   info(...args: unknown[]): void {
     if (args.length > 1) {
-      args[0] = colors.green(String(args[0]))
+      args[0] = chalk.green(String(args[0]))
     }
     console.log(...args)
   },
   error(category: string, ...args: unknown[]): void {
     if (args.length) {
-      args[0] = colors.red(String(args[0]))
-      console.error(colors.red('ERROR'), colors.red(category), ...args)
+      args[0] = chalk.red(String(args[0]))
+      console.error(chalk.red('ERROR'), chalk.red(category), ...args)
       return
     }
-    console.error(colors.red(category))
+    console.error(chalk.red(category))
   },
   warn(category: string, ...args: unknown[]): void {
     if (args.length) {
-      args[0] = colors.yellow(String(args[0]))
-      console.warn(colors.yellow('WARN'), colors.yellow(category), ...args)
+      args[0] = chalk.yellow(String(args[0]))
+      console.warn(chalk.yellow('WARN'), chalk.yellow(category), ...args)
     }
-    console.warn(colors.yellow(category))
+    console.warn(chalk.yellow(category))
   },
   trace(category: string, ...args: unknown[]): void {
     if (TRACE_ENABLED) {
-      console.log(colors.gray('TRACE'), colors.gray(category), ...args)
+      console.log(chalk.gray('TRACE'), chalk.gray(category), ...args)
     }
   },
 }

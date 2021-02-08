9238f5467a6f8a65085ab149799584a75f2c07cc
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-02-08 13:15:30 +0100

    feat: implement setting of authorization passwords

--- src/set-password.ts
+++ src/set-password.ts
@@ -7,6 +7,8 @@ import {
   toolOptionDefinitions,
   v2OptionDefinitions,
 } from './env'
+import {readFileSync} from 'fs'
+import {getV1Authorizations, postPassword} from './v2/v2-authv1-api'
 
 const localOptions = {
   usersFile: '',
@@ -14,26 +16,48 @@ const localOptions = {
 }
 
 async function main(): Promise<void> {
-  // const {usersFile, defaultPassword} = localOptions
-  // logger.info('Trying to delete buckets: ', bucketNames)
-  // const buckets = await getBuckets()
-  // const deleted: Record<string, boolean> = {}
-  // for (const bucket of buckets) {
-  //   if (bucketNames.indexOf(bucket.name) >= 0) {
-  //     deleted[bucket.name] = true
-  //     try {
-  //       await deleteBucket(bucket.id as string)
-  //       logger.info(bucket.name, 'bucket deleted')
-  //     } catch (e) {
-  //       logger.error('v2api', bucket.name, 'bucket cannot be deleted:', e)
-  //     }
-  //   }
-  // }
-  // bucketNames.forEach(x => {
-  //   if (!deleted[x]) {
-  //     logger.warn(`${x} bucket skipped, not found`)
-  //   }
-  // })
+  if (!localOptions.usersFile) {
+    throw new Error('Missing --users-file!')
+  }
+  const users = JSON.parse(
+    readFileSync(localOptions.usersFile, {encoding: 'utf8'})
+  )
+  if (!Array.isArray(users)) {
+    throw new Error('Invalid content of users-file, array expected!')
+  }
+
+  const userToAuthId = (await getV1Authorizations()).reduce((acc, v1Auth) => {
+    acc[v1Auth.token] = v1Auth.id as string
+    return acc
+  }, {} as Record<string, string>)
+  logger.info(`--- Setting passwords for users[${users.length}] ---`)
+  for (const user of users) {
+    if (user && user.name) {
+      const authorizationId = userToAuthId[user.name]
+      if (!authorizationId) {
+        logger.warn(user.name, `is ignored, no existing authorization found!`)
+        continue
+      }
+      try {
+        if (user.password) {
+          await postPassword(user.authorizationId, user.password)
+          logger.info(user.name, `: password set from users-file`)
+        } else if (localOptions.defaultPassword) {
+          await postPassword(user.authorizationId, localOptions.defaultPassword)
+          logger.info(user.name, `: now uses default password`)
+        } else {
+          logger.warn(user.name, `: user is ignored, no password specified`)
+        }
+      } catch (e) {
+        logger.error(user.name, `: unable to set password`, e)
+      }
+      continue
+    }
+    logger.warn(
+      'ignored',
+      `user entry ${JSON.stringify(user)} is not recognized`
+    )
+  }
 }
 
 const options = {

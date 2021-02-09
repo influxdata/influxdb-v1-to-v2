f74d08232a1c45c379c99d75e6dfec6f909eb5dc
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-02-09 08:09:07 +0100

    feat: validate that either v1 connection or meta file is supplied

--- src/v1/options.ts
+++ src/v1/options.ts
@@ -1,4 +1,5 @@
 import {option} from '../env'
+import logger from '../util/logger'
 
 /** InfluxDB v1 options */
 export const v1Options = {
@@ -8,8 +9,29 @@ export const v1Options = {
   metaDumpFile: '',
 }
 
+function v1OptionsValidator(): boolean {
+  if (v1Options.metaDumpFile) {
+    logger.info(`Using ${v1Options.metaDumpFile} to provide v1 inputs`)
+    return true
+  }
+  for (const option of ['url', 'user', 'password']) {
+    if (!(v1Options as Record<string, unknown>)[option]) {
+      logger.error(`Missing required option --v1-${option}`)
+      return false
+    }
+  }
+  return true
+}
+
 export const v1OptionDefinitions = [
-  option('v1-url', v1Options, 'url', 'V1_INFLUX_URL', 'source base URL'),
+  option(
+    'v1-url',
+    v1Options,
+    'url',
+    'V1_INFLUX_URL',
+    'source base URL',
+    v1OptionsValidator
+  ),
   option('v1-user', v1Options, 'user', 'V1_INFLUX_USER', 'source user'),
   option(
     'v1-password',

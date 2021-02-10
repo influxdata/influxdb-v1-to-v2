d07502b5da5230a705d13d43d97bd536d4704536
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-02-10 09:27:04 +0100

    feat: extract developement env vars into a file

---
+++ scripts/env-dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#v1 params
+export V1_INFLUX_URL=http://localhost:8087
+export V1_INFLUX_USER=admin
+export V1_INFLUX_PASSWORD=changeit
+#export V1_META_DUMP_FILE=
+
+#v2 params
+export INFLUX_URL=http://localhost:9999
+export INFLUX_TOKEN=my-token
+export INFLUX_ORG=my-org
+
+#tool params
+export OUT_V1_META=v1Meta.json
